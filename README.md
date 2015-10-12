6f99f1d941333ae6716d7b4d7dc414a10b03424a
Valentin Deville <contact@valentin-deville.eu>
2015-10-12 22:58:28 +0200

    Finish

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Valentin Deville
+
+Tableau périodique des éléments responsive à l'aide de bootstrap.
+
+Environ 4heures de travail. (Débutant).
---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tableau périodique des élements</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+    <link rel="stylesheet" href="css/custom.css">
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<h1 class="text-center">Tableau périodique des éléments</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>Groupe<i class="glyphicon glyphicon-arrow-right"></i><br />Période<br /><i class="glyphicon glyphicon-arrow-down"></i></p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>1<br />IA</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>2<br />IIA</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide-haut"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>18<br />VIIIA</p></div>
+    </div>
+    <div class="row">
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>1</p></div>
+        <div class="col-xs-1 carre" id="vert"><p>Hydrogène</p><p>1</p><p>H</p></div>
+        <div class="col-xs-4 hidden-md hidden-sm hidden-xs" id="tuto">
+            <div class="row">
+                <div class="col-xs-12">
+                    <p><i class="glyphicon glyphicon-arrow-left"></i> nom de l'élement</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <p><i class="glyphicon glyphicon-arrow-left"></i> nnuméro atomique</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <p><i class="glyphicon glyphicon-arrow-left"></i> symbole chimique</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>13<br />IIA</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>14<br />IVA</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>15<br />VA</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>16<br />VIA</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>17<br />VIIA</p></div>
+        <div class="col-xs-1 carre" id="bleu"><p>Hélium</p><p>2</p><p>He</p></div>
+    </div>
+    <div class="row">
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>2</p></div>
+        <div class="col-xs-1 carre" id="rouge"><p>Lythium</p><p>3</p><p>Li</p></div>
+        <div class="col-xs-1 carre" id="beige"><p>Bérillyum</p><p>4</p><p>Be</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 carre" id="caquis"><p>Bore</p><p>5</p><p>B</p></div>
+        <div class="col-xs-1 carre" id="vert"><p>Carbone</p><p>6</p><p>C</p></div>
+        <div class="col-xs-1 carre" id="vert"><p>Azote</p><p>7</p><p>N</p></div>
+        <div class="col-xs-1 carre" id="vert"><p>Oxygène</p><p>8</p><p>O</p></div>
+        <div class="col-xs-1 carre" id="jaune"><p>Fluor</p><p>9</p><p>F</p></div>
+        <div class="col-xs-1 carre" id="bleu"><p>Néon</p><p>10</p><p>Ne</p></div>
+    </div>
+    <div class="row">
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>3</p></div>
+        <div class="col-xs-1 carre" id="rouge"><p>Sodium</p><p>11</p><p>Na</p></div>
+        <div class="col-xs-1 carre" id="beige"><p>Magnésium</p><p>12</p><p>Mg</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>3<br />IIIB</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>4<br />IVB</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>5<br />VB</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>6<br />VIB</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>7<br />VIIB</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>8<br /></p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>9<br />VIIIB</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>10<br /></p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>11<br />IB</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"><p>12<br />IIB</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Aluminium</p><p>13</p><p>Al</p></div>
+        <div class="col-xs-1 carre" id="caquis"><p>Silicium</p><p>14</p><p>Si</p></div>
+        <div class="col-xs-1 carre" id="vert"><p>Phosphore</p><p>15</p><p>P</p></div>
+        <div class="col-xs-1 carre" id="vert"><p>Soufre</p><p>16</p><p>S</p></div>
+        <div class="col-xs-1 carre" id="jaune"><p>Chlore</p><p>17</p><p>Cl</p></div>
+        <div class="col-xs-1 carre" id="bleu"><p>Argon</p><p>18</p><p>Ar</p></div>
+    </div>
+    <div class="row">
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>4</p></div>
+        <div class="col-xs-1 carre" id="rouge"><p>Potasium</p><p>19</p><p>K</p></div>
+        <div class="col-xs-1 carre" id="beige"><p>Calcium</p><p>20</p><p>Ca</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 carre" id="rose"><p>Scandium</p><p>21</p><p>Sc</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Titane</p><p>22</p><p>Ti</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Vanadium</p><p>23</p><p>V</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Chrome</p><p>24</p><p>Cr</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Manganèse</p><p>25</p><p>Mn</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Fer</p><p>26</p><p>Fe</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Cobalt</p><p>27</p><p>Co</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Nickel</p><p>28</p><p>Ni</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Cuivre</p><p>29</p><p>Cu</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Zinc</p><p>30</p><p>Zn</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Gallium</p><p>31</p><p>Ga</p></div>
+        <div class="col-xs-1 carre" id="caquis"><p>Germanium</p><p>32</p><p>Ge</p></div>
+        <div class="col-xs-1 carre" id="caquis"><p>Arsenic</p><p>33</p><p>As</p></div>
+        <div class="col-xs-1 carre" id="vert"><p>Sélénium</p><p>34</p><p>Se</p></div>
+        <div class="col-xs-1 carre" id="jaune"><p>Brome</p><p>35</p><p>Br</p></div>
+        <div class="col-xs-1 carre" id="bleu"><p>Krypton</p><p>36</p><p>Kr</p></div>
+    </div>
+    <div class="row">
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>5</p></div>
+        <div class="col-xs-1 carre" id="rouge"><p>Rubidium</p><p>37</p><p>Rb</p></div>
+        <div class="col-xs-1 carre" id="beige"><p>Strontium</p><p>38</p><p>Sr</p></div>
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs" id="vide"></div>
+        <div class="col-xs-1 carre" id="rose"><p>Yttrium</p><p>39</p><p>Y</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Zirconium</p><p>40</p><p>Zr</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Niobium</p><p>41</p><p>Nb</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Molybdène</p><p>42</p><p>Mo</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Technétium</p><p>43</p><p>Tc</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Ruthénium</p><p>44</p><p>Ru</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Rhodium</p><p>45</p><p>Rh</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Palladium</p><p>46</p><p>Pd</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Argent</p><p>47</p><p>Ag</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Cadmium</p><p>48</p><p>Cd</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Indium</p><p>49</p><p>In</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Etain</p><p>50</p><p>Sn</p></div>
+        <div class="col-xs-1 carre" id="caquis"><p>Antimoine</p><p>51</p><p>Sb</p></div>
+        <div class="col-xs-1 carre" id="caquis"><p>Tellure</p><p>52</p><p>Te</p></div>
+        <div class="col-xs-1 carre" id="jaune"><p>Iode</p><p>53</p><p>I</p></div>
+        <div class="col-xs-1 carre" id="bleu"><p>Xénon</p><p>54</p><p>Xe</p></div>
+    </div>
+    <div class="row">
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>6</p></div>
+        <div class="col-xs-1 carre" id="rouge"><p>Césium</p><p>55</p><p>Cs</p></div>
+        <div class="col-xs-1 carre" id="beige"><p>Baryum</p><p>56</p><p>Ba</p></div>
+        <div class="col-xs-2 carre2" id="rosef"><p>Lanthanides</p><p>57-71</p><p>-</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Hafnium</p><p>72</p><p>Hf</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Tantale</p><p>73</p><p>Ta</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Tangstène</p><p>74</p><p>W</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Rhénium</p><p>75</p><p>Re</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Osnium</p><p>76</p><p>Os</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Iridium</p><p>77</p><p>Ir</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Platine</p><p>78</p><p>Pt</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Or</p><p>79</p><p>Au</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Mercure</p><p>80</p><p>Hg</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Thallium</p><p>81</p><p>Tl</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Plomb</p><p>82</p><p>Pb</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Bismuth</p><p>83</p><p>Bi</p></div>
+        <div class="col-xs-1 carre" id="caquis"><p>Polonium</p><p>84</p><p>Po</p></div>
+        <div class="col-xs-1 carre" id="jaune"><p>Astate</p><p>85</p><p>At</p></div>
+        <div class="col-xs-1 carre" id="bleu"><p>Radon</p><p>86</p><p>Rn</p></div>
+    </div>
+    <div class="row">
+        <div class="col-xs-1 hidden-md hidden-sm hidden-xs"><p>7</p></div>
+        <div class="col-xs-1 carre" id="rouge"><p>Francium</p><p>87</p><p>Fr</p></div>
+        <div class="col-xs-1 carre" id="beige"><p>Radium</p><p>88</p><p>Ra</p></div>
+        <div class="col-xs-2 carre2" id="violet"><p>Actinides</p><p>89-103</p><p>-</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Rutherfodium</p><p>104</p><p>Rf</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Dubnium</p><p>105</p><p>Db</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Seaborgium</p><p>106</p><p>Sg</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Bohrium</p><p>107</p><p>Bh</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Hassium</p><p>108</p><p>Hs</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Meitnérium</p><p>109</p><p>Mt</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Darmstadium</p><p>110</p><p>Ds</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Roentgenium</p><p>111</p><p>Rg</p></div>
+        <div class="col-xs-1 carre" id="rose"><p>Copernicium</p><p>112</p><p>Cn</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Unumtrium</p><p>113</p><p>Uut</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Flérovium</p><p>114</p><p>Fl</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Ununpentium</p><p>115</p><p>Uup</p></div>
+        <div class="col-xs-1 carre" id="gris"><p>Livermorium</p><p>116</p><p>Lv</p></div>
+        <div class="col-xs-1 carre" id="jaune"><p>Ununseptium</p><p>117</p><p>Uus</p></div>
+        <div class="col-xs-1 carre" id="bleu"><p>Ununoctium</p><p>118</p><p>Uuo</p></div>
+    </div>
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
